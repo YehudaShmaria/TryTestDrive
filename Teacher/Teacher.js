f21b77365c8e5f2aef7d62ba9109a5b290d47712
yehuda <yehudasworkp@gmail.com>
2020-12-03 14:36:52 +0200

    yehuda

--- Teacher/Teacher.js
+++ Teacher/Teacher.js
@@ -1,3 +1,27 @@
+var Url = "https://data.gov.il/dataset/3fc54b81-25b3-4ac7-87db-248c3e1602de/resource/72bd51be-512b-4430-b2d2-f3295c90e569/download/72bd51be-512b-4430-b2d2-f3295c90e569.xml";
 $("document").ready(function () {
     $("#Gamp").fadeIn(3000);
 });
+
+function Getsearching(){
+    $('#searching').fadeIn(3000)
+
+    var xml = new XMLHttpRequest();
+    xml.open("GET", Url, true);
+    xml.onreadystatechange = function () {
+    var alladta = xml.responseText;
+    var parser = new DOMParser();
+    var xmldoc = parser.parseFromString(alladta,"text/xml")
+    var x =  xmldoc.getElementsByTagName("שם_ישוב");
+    for(i=0;i<x.length;i++)
+    {
+      $('.Loction').append($("<option></option>")
+                 .attr("value", i.key)
+                 .text(x[i].textContent));
+     }
+    };
+   
+    xml.send();
+}
+
+
