995b105f8fdf3c8408656c37c4342bb6101e6355
yehuda <yehudasworkp@gmail.com>
2021-01-11 23:30:35 +0200

    Test1

--- Contact/Contact.js
+++ Contact/Contact.js
@@ -1,13 +1,16 @@
-function sendEmail() {
-	Email.send({
-	Host: "smtp.gmail.com",
-	Username : "yehudas1999@gmail.com",
-	Password : "207193780",
-	To : 'yehudas1999@gmail.com',
-	From : "yehudas1999@gmail.com",
-	Subject : "Hello World",
-	Body : "כמה טוב לי שאני יהודי",
-	}).then(
-		message => alert("mail sent successfully")
-	);
-}
+var Name = document.getElementById('Name');
+var body = document.getElementById('body')
+function sendEmail() { 
+	Email.send({ 
+	  Host: "smtp.gmail.com", 
+	  Username: "yehudas1999@gmail.com", 
+	  Password: "207193780", 
+	  To: 'yehudas1999@gmail.com', 
+	  From: "yehudas1999@gmail.com", 
+	  Subject: `${Name} is Send A mail`, 
+	  Body: `${body}`, 
+	}) 
+	  .then(function (message) { 
+	  alert("mail sent successfully") 
+	  }); 
+	} 
